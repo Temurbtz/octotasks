d6d271683d5d599cc354c7eb89d3cc7107d50cba
Temurbekjonbtz <56353734+Temurbekjonbtz@users.noreply.github.com>
2023-02-19 20:49:10 +0500

    initial commit

---
+++ src/main/java/com/tasks/octotasks/OctotasksApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.octotasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctotasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctotasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasks/octotasks/OctotasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.octotasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctotasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
